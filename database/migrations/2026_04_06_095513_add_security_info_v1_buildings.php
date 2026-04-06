de224df2ff200c2d3b7fa86cfb7aafbe19c797c4
Mahmoud Aloul <mhmudaloul@gmail.com>
2026-04-06 09:55:35 +0300

    Auto-update: 2026-04-06 09:55:35

---
+++ database/migrations/2026_04_06_095513_add_security_info_v1_buildings.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+    {
+        Schema::table('buildings', function (Blueprint $table) {
+            $table->string('security_info_v1')->nullable()->after('id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('buildings', function (Blueprint $table) {
+            $table->dropColumn('security_info_v1');
+
+        });
+    }
+};
