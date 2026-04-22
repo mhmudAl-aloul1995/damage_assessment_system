51e72f9eb2822399407cddd82e4346b8a0e38f60
Mahmoud Aloul <mhmudaloul@gmail.com>
2026-04-22 16:19:23 +0300

    Auto-update: 2026-04-22 16:19:22

--- app/Http/Controllers/Report/reportController.php
+++ app/Http/Controllers/Report/reportController.php
@@ -121,6 +121,7 @@ class reportController extends Controller
 
     public function exportCommulative(Request $request)
     {
+        set_time_limit(-1);
         $startDate = $request->input('start_date', now()->subDays(30)->toDateString());
         $endDate = $request->input('end_date', now()->toDateString());
 
