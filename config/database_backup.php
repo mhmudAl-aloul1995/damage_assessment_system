096dc50ae78215b2e5801a50520e59037c48cac7
Mahmoud Aloul <mhmudaloul@gmail.com>
2026-04-27 23:49:20 +0300

    Auto-update: 2026-04-27 23:49:19

--- config/database_backup.php
+++ config/database_backup.php
@@ -1,16 +1,21 @@
 <?php
 
 return [
-    'disk' => env('DB_BACKUP_DISK', 'local'),
+
+    'connection' => env('DB_BACKUP_CONNECTION', env('DB_CONNECTION', 'mysql')),
+
     'path' => env('DB_BACKUP_PATH', 'backups/database'),
-    'schedule_time' => env('DB_BACKUP_TIME', '00:00'),
-    'connection' => env('DB_BACKUP_CONNECTION', env('DB_CONNECTION', 'sqlite')),
-    'host' => env('DB_BACKUP_HOST'),
-    'port' => env('DB_BACKUP_PORT'),
-    'database' => env('DB_BACKUP_DATABASE'),
-    'username' => env('DB_BACKUP_USERNAME'),
-    'password' => env('DB_BACKUP_PASSWORD'),
+
     'mysqldump_binary' => env('DB_BACKUP_MYSQLDUMP_BINARY'),
+
     'mariadb_dump_binary' => env('DB_BACKUP_MARIADB_DUMP_BINARY'),
+
     'pg_dump_binary' => env('DB_BACKUP_PG_DUMP_BINARY'),
+
+    'host' => env('DB_HOST'),
+    'port' => env('DB_PORT'),
+    'database' => env('DB_DATABASE'),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
+
 ];
