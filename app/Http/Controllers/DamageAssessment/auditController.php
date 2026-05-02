a9a77e133f1662b6a27b9dff3245658b2cd80f77
Mahmoud Aloul <mhmudaloul@gmail.com>
2026-05-02 14:20:23 +0300

    Auto-update: 2026-05-02 14:20:23

--- app/Http/Controllers/DamageAssessment/auditController.php
+++ app/Http/Controllers/DamageAssessment/auditController.php
@@ -2898,15 +2898,22 @@ class auditController extends Controller
                 });
             }
 
-            // استثناء المباني المعيّنة مسبقاً
             $query->whereNotExists(function ($q) {
                 $q->selectRaw('1')
-                    ->from('assigned_assessment_users as aau')
-                    ->whereColumn('aau.building_id', 'buildings.objectid')
-                    ->whereIn('aau.type', [
-                        'engineer',
-                        'lawyer',
-                    ]);
+                    ->from('building_statuses as bs')
+                    ->join('assessment_statuses as s', 'bs.status_id', '=', 's.id')
+                    ->whereColumn('bs.building_id', 'buildings.objectid')
+                    ->whereIn('s.name', [
+                        'assigned_to_engineer',
+                        'assignedto_engineer',
+                        'assigned_to_lawyer',
+                        'assignedto_lawyer',
+                    ])
+                    ->whereRaw('bs.updated_at = (
+            SELECT MAX(bs2.updated_at)
+            FROM building_statuses bs2
+            WHERE bs2.building_id = bs.building_id
+        )');
             });
 
             $housingStatusCounts = function ($row): array {
