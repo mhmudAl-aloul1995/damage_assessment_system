d5a3acbc10880f633f0d860a0a13d557d2b17560
Mahmoud Aloul <mhmudaloul@gmail.com>
2026-05-13 20:17:45 +0300

    Auto-update: 2026-05-13 20:17:44

--- app/Http/Controllers/DamageAssessment/auditController.php
+++ app/Http/Controllers/DamageAssessment/auditController.php
@@ -2805,7 +2805,7 @@ class auditController extends Controller
                 'engineerStatus.status',
                 'lawyerStatus.status',
             ])
-                 ->whereIn('globalid', $globalIds)
+                 ->whereNotIn('globalid', $globalIds)
                 ->where('field_status', 'COMPLETED');
 
             $engineerIds = $this->filterValues($request, 'engineer_id');
