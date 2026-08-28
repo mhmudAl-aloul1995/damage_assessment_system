3bf0c17d517aa49a44f2bd1119e09ec4e5c483f9
Mahmoud Aloul <mhmudaloul@gmail.com>
2026-08-28 14:23:06 +0300

    Auto-update: 2026-08-28 14:23:06

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Schema;
-
+//اا
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
